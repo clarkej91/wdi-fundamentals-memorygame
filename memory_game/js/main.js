8c9c4de6dfd56297b09c75e9e3a5bf72c15e4387
Jacob  Clarke <jacobclarke@Jacobs-MacBook-Pro.local>
2019-03-08 12:24:58 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
-console.log("up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [0];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardTwo = [2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){ 
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
