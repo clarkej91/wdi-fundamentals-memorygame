13f19e57c7d0ba288455e878b61696379df73d1a
Jacob  Clarke <jacobclarke@Jacobs-MacBook-Pro.local>
2019-03-13 14:49:05 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-console.log(flipCard);
-console.log(flipCard[0]);
-console.log(flipCard[2]);
-
-var checkForMatch = function() {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
   console.log("Sorry, try again.");
-};
-};
-
-var flipCard = function(cardId) {
-	var cardOne = [0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen" + cards[cardId]);
-
-var cardTwo = [2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king" + cards[cardId]);
-console.log(checkForMatch);
 }
+};
+var flipCard = function (cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+if (cardsInPlay.length >= 2) {
+	checkForMatch();
+}
+};
+flipCard(0);
+flipCard(2);
+
